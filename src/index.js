c2099036881b71b58fbad7c4164802a4a0acf261
Joseph Yusufov <jyusufov00@stuy.edu>
2020-04-06 23:30:06 -0400

    did some shit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
+
+// // Module 1
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// ReactDOM.render(<div><h1>Hello World</h1><p>Thomas Ghita</p></div>, document.getElementById("root"));
+
+
+//  var myNewP = document.createElement("p");
+//  myNewP.innerHTML = "This is my new P. Its pretty nice";
+
+// // Module 2
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// function MyApp() {
+// 	return 	(
+// 		<ul>
+// 			<li>Daniel</li>
+// 			<li>Timone</li>
+// 			<li>musha</li>
+// 		</ul>
+// 	);
+// }
+
+// ReactDOM.render(
+// 	<MyApp/>,
+// 	document.getElementById("root")
+// );
+
+// // Module 3
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// function MyInfo () {
+// 	return (
+// 		<div>
+// 			<h1>Joseph Yusufov</h1>
+// 			<p>I like to play hockey, make shashlik, and play the occasional Warzone.</p>
+// 			<ul>
+// 				<li>Tokyo, japan</li>
+// 				<li>Barbados</li>
+// 				<li>Timone's House</li>
+// 			</ul>
+// 		</div>
+// 		);
+// }
+
+// // Module 4
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import MyInfo from "./components/MyInfo.js";
+
+// ReactDOM.render(<MyInfo/>, document.getElementById("root"));
+
+
+// Module 5
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from "./components/App.js";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App/>, document.getElementById("root"));
